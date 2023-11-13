65525546e39a1307c5152e78472196bd2a232c97
ch052877 <christyanmend@gmail.com>
2023-11-13 05:28:05 -0300

    poo examples 1

---
+++ poo/Pessoa.py
@@ -0,0 +1,9 @@
+class Pessoa :
+    pass
+
+
+pessoa1 = Pessoa()
+
+pessoa1.nome="João"
+print(pessoa1.nome)
+print(pessoa1)
---
+++ poo/app.py
@@ -0,0 +1,6 @@
+from Pessoa import Pessoa
+
+pessoa2 = Pessoa()
+
+pessoa2.name="jason"
+print(pessoa2.name)
