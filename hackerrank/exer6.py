fe330567cbd355fe1d744eb84c957908d6d21bc1
ch052877 <christyanmend@gmail.com>
2024-04-27 12:37:15 -0300

    exer 7

--- hackerrank/exer6.py
+++ hackerrank/exer6.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
     contador = 0
-    while n > contador:
-        print("test")
+    while n != contador:
+        contador += 1
+        print(contador, end='')
