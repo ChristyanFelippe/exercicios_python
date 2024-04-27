67a04858101ebe0636219fdd29f4478d8e751edf
ch052877 <christyanmend@gmail.com>
2024-04-27 01:31:08 -0300

    exer 5 and 6

--- hackerrank/exer5.py
+++ hackerrank/exer5.py
@@ -1,9 +1,14 @@
 def is_leap(year):
     leap = False
-    
-    # Write your logic here
-    
-    return leap
+    isLeapBy400 = year % 400
+    isLeapBy4 = year % 4
+    isLeapUnless100 = year % 100
+    if isLeapBy400 == 0:
+        return True
+    elif (isLeapBy4 == 0) and (isLeapUnless100 != 0):
+        return True
+    else:
+        return leap
 
 year = int(input())
 print(is_leap(year))
---
+++ hackerrank/exer6.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    contador = 0
+    while n > contador:
+        print("test")
